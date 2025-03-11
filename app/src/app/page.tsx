cc68b6ce35b61848273c9e5026dbd1ca76f04829
James Crowe <jamesbc2004@gmail.com>
2025-03-11 18:50:52 -0500

    Adding back as pngs

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,5 @@
-//import Image from 'next/image';
-//import jamesCroweImage from '../images/james-crowe-1.jpg';
+import Image from 'next/image';
+import jamesCroweImage from '../images/james-crowe-1.png';
 
 export default function Home() {
   return (
@@ -33,7 +33,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        {/* <div className="flex-shrink-0">
+        <div className="flex-shrink-0">
           <Image
             src={jamesCroweImage}
             alt="[James Crowe]"
@@ -41,7 +41,7 @@ export default function Home() {
             height={300}
             className="rounded"
           />
-        </div> */}
+        </div>
       </div>
     </main>
   );
