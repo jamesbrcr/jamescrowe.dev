24637126f1eb8317f244fbca294867c2b3a4ab37
James Crowe <jamesbc2004@gmail.com>
2025-03-11 19:17:49 -0500

    fixing basepath

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? 'jamescrowe.dev' : '', 
-  assetPrefix: isProd ? 'jamescrowe.dev/' : '', 
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
+  /* config options here */
 };
 
 export default nextConfig;
