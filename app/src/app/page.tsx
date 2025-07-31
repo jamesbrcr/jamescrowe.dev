103742d2ee93a06cce7a2a11fe49cda1fa23291e
James Crowe <jamesbc2004@gmail.com>
2025-07-30 22:14:17 -0700

    apos fixes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
       <main className="relative z-10 flex min-h-screen items-center justify-center p-6 text-white">
         <div className="bg-[#1e1e1e] p-10 rounded-lg shadow-[0_15px_35px_rgba(0,0,0,0.9)] max-w-4xl flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8">
           <div className="flex-1 space-y-6 text-center md:text-left">
-            <h1 className="text-4xl font-bold text-red-500">Hey, I’m James</h1>
+            <h1 className="text-4xl font-bold text-red-500">Hey, I&apos;m James</h1>
             <p className="text-lg text-gray-200">
               I build fun stuff.
             </p>
@@ -96,7 +96,7 @@ export default function Home() {
                 rel="noopener noreferrer"
                 className="text-red-400 hover:text-red-300"
               >
-                UW–Madison
+                UW Madison
               </a>{' '}
               studying Comp Sci + Data Sci.
             </p>
@@ -107,7 +107,7 @@ export default function Home() {
                 rel="noopener noreferrer"
                 className="px-6 py-2 bg-[rgba(128,128,128,0.2)] text-white rounded-lg hover:bg-[#353042] transition-colors"
               >
-                Here's my resume
+                Here&apos;s my resume
               </a>
             </div>
           </div>
